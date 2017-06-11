adb119ae953eb1c22ce712b441b26b735a8e025b
danichmur <muraveyko_d@mail.ru>
2017-06-11 16:30:22 +0300

    client

---
+++ server/client/src/components/SomeProject/Project.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Paper,Divider,Chip, MenuItem }  from 'material-ui';
+import { Grid,Row,Col } from 'react-flexbox-grid';
+import {List, ListItem} from 'material-ui/List';
+import { Route, Link } from 'react-router-dom';
+import ApiQueries from '../ApiQueries';
+
+export default class AllProjects extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      project:null,
+    }
+  };
+
+
+  componentDidMount(){
+    ApiQueries.getOneProject(this.props.match.params.id, (data => {
+      this.setState({ projects: data });
+    }));
+  };
+  render() {
+		return (
+      <Grid fluid>
+        <Row>
+          <Col xs={12} smOffset={2} sm={8} md={8} mdOffset={2} lgOffset={3} lg={6}>
+          Project
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
