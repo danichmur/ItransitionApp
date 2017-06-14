fa82c594de33c7ce872759f26ad066c705303537
danichmur <muraveyko_d@mail.ru>
2017-06-14 21:35:59 +0300

    updated tags on project

--- server/app/controllers/tags_controller.rb
+++ server/app/controllers/tags_controller.rb
@@ -8,12 +8,18 @@ class TagsController < ApplicationController
   
   def tags_on_project
     project = Project.find(params[:project_id])
-    
-    if  project.tags.update(tags_params)
-      render status: :ok
-    else 
-      render status: :unprocessable_entity
+    hash = tags_params
+    hash = JSON.parse(hash) if hash.is_a?(String)
+    project.tags.clear
+    hash[:tags].each do |tag|
+      if(Tag.exists?(value: tag[:value]) != true)
+        t = Tag.create(value: tag[:value])
+        project.tags << t
+      else
+        project.tags << Tag.find_by(value: tag[:value])
+      end 
     end
+    render status: :ok
   end
   
   def tags_params
